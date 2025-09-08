49e9f543ddc90690db909ead42e75c5e1cb1d078
Amb-general <ambassadorgentle@gmail.com>
2025-09-08 13:49:26 +0100

    python file read

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+#This is a python file for addition subtraction multiplication of two numbers
+number1= 10
+number2= 5
+sum=number1 + number2
+minus=number1 - number2
+multi= number1 * number2
+
+print (f"Addition of {number1} and {number2} is {sum}")
+print (f"Subtraction of {number1} and {number2} is {minus}")
+print (f"Multiplication of {number1} and {number2} is {multi}")
